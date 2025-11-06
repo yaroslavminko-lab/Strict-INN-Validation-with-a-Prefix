ef6a8bf0f6da83ac33228cbd832e6a8a10a764ca
YarioM <yaroslav.minko@innowise.com>
2025-11-06 11:10:03 +0300

    Initial commit

---
+++ Strict INN Validation with a Prefix/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strict_INN_Validation_with_a_Prefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Strict INN Validation with a Prefix/Validation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strict_INN_Validation_with_a_Prefix
+{
+    class Validation
+    {
+    }
+}
